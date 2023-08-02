b70f5b6044a4af16538261d7e85663de84bd284a
Selma19 <selmakouaiche@gmail.com>
2023-08-02 15:19:43 +0200

    Add files via upload

--- OU_data.py
+++ OU_data.py
@@ -3,108 +3,79 @@ import numpy as np
 from math import *
 import cmath as cm
 import random as rd
-from scipy.signal import fftconvolve
-import matplotlib.animation as animation
 
 
 
-class Animation:
-	"""docstring for Animation"""
-	def __init__(self,data_dir,c):
-		#initialize the figure
-		self.fig, self.ax = plt.subplots()
-		self.ax.set_aspect('equal')
-		self.line, = self.ax.plot([],[])
-		self.x = np.real(data_dir)
-		self.y = np.imag(data_dir)
-		b1 = np.max(abs(self.x))
-		b2 = np.max(abs(self.y))
-		self.ax.set_xlim(-b1,b1)
-		self.ax.set_ylim(-b2,b2)
-		self.nb_frame = len(data_dir)
-		theta = np.linspace(0,2*np.pi,1000)
-		self.ax.plot(c*np.cos(theta),c*np.sin(theta))
-	
-	#i is the frame number
-	def animate(self,i):
-		self.line.set_data([0,self.x[i]],[0,self.y[i]])
-		return self.line,
+def funcJs1(t):
+	res = np.zeros(t.shape)
+	for i in range(len(res)):
+		if t[i] > 0:
+			res[i] = np.exp(-t[i])
+	return res
 
-	def make_anim(self):
-		ani = animation.FuncAnimation(self.fig,self.animate,frames=self.nb_frame,interval=1,blit=True,repeat=False)
-		ani.save('/Users/Selma/Desktop/PhD/data/OU_data/animation/OU_data.gif',writer='pillow')
+class Generate_data:
+	"""
+	refresh_rate in Hertz
+	"""
+	def __init__(self, refresh_rate=120): 
+		self.dt_obs = 1000/refresh_rate #sampling duration in ms
+		self.gamma_OU = 0.05/self.dt_obs
+		self.sigma_OU = np.sqrt(2*self.gamma_OU)
+		self.dt_sim = min(1/self.gamma_OU,self.dt_obs)*0.1 # the simulation dt must be lower than observation dt and 1/gamma (in ms)
+		self.nb_alpha = 10
+		self.nb_frame = 300
+		self.T = self.nb_alpha * self.nb_frame * self.dt_obs #the duration of the simulation in ms
+		self.coh = 1
+		self.epsilon_data = 0.05 #strength of the OU noise in point direction
+		self.epsilon_JS = 0.05 #strength of the JS noise
+		self.nb_pt = floor(self.T/self.dt_obs) #number of measurements
 
+	def OU_process(self):
+		#making an OU process
+		nb = floor(self.T/self.dt_sim)
+		x = np.zeros(nb)
+		eta = np.random.normal(loc=0,scale=1,size=nb-1) # bruit blanc gaussien 
+		for t in range(1,nb):
+			x[t] = (1-self.gamma_OU*self.dt_sim)*x[t-1] + self.sigma_OU*np.sqrt(self.dt_sim)*eta[t-1]
+		return x
 
+	def get_data(self):
+		alpha = np.random.random(self.nb_alpha)*np.pi*2
+		d1 = self.OU_process()
+		d2 = self.OU_process()
+		data_dir = np.zeros(self.nb_pt,dtype=complex)
+		nom_dir = np.zeros(self.nb_pt,dtype=complex)
+		#generating data
+		for k,a in enumerate(alpha):
+			nom_dir[k*self.nb_frame:(k+1)*self.nb_frame] = self.coh*cm.exp(a*1j)
+			data_dir[k*self.nb_frame:(k+1)*self.nb_frame] = self.coh*cm.exp(a*1j)
+		data_dir += (d1[::floor(np.round(self.dt_obs/self.dt_sim))] + 1j*d2[::floor(np.round(self.dt_obs/self.dt_sim))]) * self.epsilon_data
+		return data_dir, nom_dir
 
+	def convolution(self,A,B):
+		t = np.linspace(-self.T/2,self.T/2,len(B))
+		return np.convolve(A(t),B,mode = 'same')*self.dt_obs
 
-def OU_process(sigma,T):
-	# making an OU process
-	gamma = sigma**2/2 # theoretical
-	dt = 0.1/gamma #time step
-	nb = floor(T/dt) #nb of time steps
-	x = np.zeros(nb) 
-	eta = np.random.normal(loc=0,scale=1,size=nb-1) # bruit blanc gaussien
-	for t in range(1,nb):
-		x[t] = (1-gamma*dt)*x[t-1] + sigma*np.sqrt(dt)*eta[t-1]
-	return x
+	def JS_1(self,data_dir,tau=0.5,lag=0): 
+		return self.convolution(lambda t:funcJs1((t-lag)/tau)/tau,data_dir)
 
+	def JS_2(self,data_dir,tau=0.5):
+		d1 = self.OU_process()
+		d2 = self.OU_process()
+		js_1 = self.JS_1(data_dir,tau=tau)
+		return  js_1 + (d1[::floor(np.round(self.dt_obs/self.dt_sim))] + 1j*d2[::floor(np.round(self.dt_obs/self.dt_sim))]) * self.epsilon_JS 
 
-# generate random data folowing the OU process 
-# alpha is the nominal direction (angle in rad)
-# noise is the OU process  
-
-def get_data():
-	nb_alpha = 10
-	alpha = np.random.random(nb_alpha)*np.pi*2
-	list_size = np.random.randint(250,300,nb_alpha)
-	T = nb_alpha * 300
-	sigma = 1
-	c = 1
-	epsilon = 0.1
-	d1 = OU_process(sigma,T)
-	d2 = OU_process(sigma,T)
-	data_dir = np.zeros(np.sum(list_size),dtype=complex)
-	D_n = np.zeros(np.sum(list_size),dtype=complex)
-	j = 0
-	#generating data
-	for a,l in zip(alpha,list_size):
-		data_dir[j:j+l] = c*cm.exp(a*1j) + (d1[j:j+l] + 1j*d2[j:j+l]) * epsilon
-		D_n[j:j+l] = c*cm.exp(a*1j) 
-		j += l
-	return data_dir, D_n
-
-
-def convolution(A,B,T,nb):
-	dt = 2*T/nb 
-	t = np.linspace(-T,T,nb) 
-	conv = np.convolve(A(t),B,mode = 'same')*dt
-	return conv
+	def JS_3(self,data_dir,tau=0.5,lag=1):
+		d1 = self.OU_process()
+		d2 = self.OU_process() 
+		js_1 = self.JS_1(data_dir,tau=tau,lag=lag)
+		return  js_1 + (d1[::floor(np.round(self.dt_obs/self.dt_sim))] + 1j*d2[::floor(np.round(self.dt_obs/self.dt_sim))]) * self.epsilon_JS 
 
 
-def JS_1(data_dir,T=100,tau=0.5 ,lag=0): 
-	return convolution(lambda t:np.exp(-(t-lag)/tau)*np.heaviside(t-lag,0.5)/tau,data_dir,T,len(data_dir))  
 
-
-def JS_2(data_dir,T=100,tau=0.5):
-	d1 = OU_process(1,40*T)
-	d2 = OU_process(1,40*T) 
-	eta = 0.02
-	js_1 = JS_1(data_dir,T=T,tau=tau)
-	return  js_1 + (d1[len(d1)-len(js_1):] + 1j*d2[len(d2)-len(js_1):]) * eta 
-
-
-def JS_3(data_dir,T=100,tau=0.5,lag=1):
-	d1 = OU_process(1,40*T)
-	d2 = OU_process(1,40*T) 
-	eta = 0.02
-	js_1 = JS_1(data_dir,T=T,tau=tau,lag=lag)
-	return  js_1 + (d1[len(d1)-len(js_1):] + 1j*d2[len(d2)-len(js_1):]) * eta 
-
-
-def J_p_fourier(D_n,J): # is not precise 
-	J_hat = np.conjugate(np.fft.fft(J))
-	D_n_hat = np.conjugate(np.fft.fft(D_n))
-	return np.fft.ifft(J_hat/D_n_hat)
+def autocorr(x):
+	result = np.correlate(x, x, mode='full')
+	return result[result.size//2:]*2/result.size
 
 
 def D_n_matrix(D_n,n):
@@ -115,23 +86,21 @@ def D_n_matrix(D_n,n):
 		mat[tau,:] = D_n[tau-n+1:tau+1][::-1]
 	return mat
 
-
 def Ker_mat(mat,Js):
 	return np.dot(np.dot(np.linalg.inv(np.dot(mat.T,mat)),mat.T),Js)
 
-
 def all_to_ker():
-	data_dir, D_n = get_data()
+	gen = Generate_data()
+	data_dir, D_n = gen.get_data()
 	delta_D = data_dir - D_n
-	dt = 200/len(data_dir)
 	n = 100 #kernel size
-	list_t = np.linspace(0,n*dt,n)
-	tau=0.5
-	lag = 1
+	list_t = np.linspace(0,n*gen.dt_obs,n)
+	tau = 10*gen.dt_obs #kernel parameter in ms
+	lag = 25*gen.dt_obs #kernel parameter in ms 
 
-	J1 = JS_1(data_dir) 
-	J2 = JS_2(data_dir) 
-	J3 = JS_3(data_dir)
+	J1 = gen.JS_1(data_dir,tau=tau)
+	J2 = gen.JS_2(data_dir,tau=tau)
+	J3 = gen.JS_3(data_dir,tau=tau,lag=lag)
 	#d_n_mat : nominal direction as a matrix
 	d_n_mat = D_n_matrix(D_n,n) #D_n[a:b]
 
@@ -147,9 +116,9 @@ def all_to_ker():
 		ratio = np.max(np.abs(ker_th))/np.max(np.abs(ker[1:-1])) # multiplicative ratio that doesn't affect the characteristic time
 		list_ratio.append(ratio)
 
-	J_p1 = np.convolve(list_ratio[0]*ker1,D_n,mode='same')*dt
-	J_p2 = np.convolve(list_ratio[1]*ker2,D_n,mode='same')*dt
-	J_p3 = np.convolve(list_ratio[2]*ker3,D_n,mode='same')*dt
+	J_p1 = np.convolve(list_ratio[0]*ker1,D_n,mode='same')*gen.dt_obs
+	J_p2 = np.convolve(list_ratio[1]*ker2,D_n,mode='same')*gen.dt_obs
+	J_p3 = np.convolve(list_ratio[2]*ker3,D_n,mode='same')*gen.dt_obs
 
 	dJ1 = J1 - J_p1
 	dJ2 = J2 - J_p2
@@ -160,6 +129,19 @@ def all_to_ker():
 	C3 = np.correlate(dJ3,delta_D,mode='full')[len(dJ3)-1:]
 	
 	return C1, C2, C3
+
+
+#auto correlation for t > 0
+def autocorr_J_th(sigma,gamma,tau,t):
+	tab1 = np.exp(-gamma*t)
+	tab2 = np.exp(-t/tau)
+	return sigma**2*((tab1+tab2)/(gamma*tau+1)+(tab2-tab1)/(gamma*tau-1))/(2*gamma)
+
+def autocorr_D_th(sigma,gamma,t):
+	return sigma**2*np.exp(-gamma*t)/gamma
+
+
+
 
 
 
